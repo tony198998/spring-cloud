35ab55b23a8646c4f28c81bccd9a420bf7e192fd
tony198998 <756903039@qq.com>
2020-03-12 16:12:50 +0800

    初始化pom，提交item工程

---
+++ cloud-item/src/main/java/com/wode/controller/ItemController.java
@@ -0,0 +1,33 @@
+package com.wode.controller;
+
+import com.wode.entity.Item;
+import com.wode.service.ItemService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/item")
+@Slf4j
+public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    /**
+     * 对外提供接口服务，查询商品信息
+     *
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/{id}")
+    public Item queryItemById(@PathVariable("id") Long id) {
+        log.info("----------"+id);
+        return this.itemService.queryItemById(id);
+    }
+
+
+}
---
+++ cloud-item/src/main/java/com/wode/entity/Item.java
@@ -0,0 +1,28 @@
+package com.wode.entity;
+
+import lombok.Data;
+
+/**
+ * 商品目录
+ */
+@Data
+public class Item {
+
+    private Long id;
+
+    private String title;
+
+    private String pic;
+
+    private String desc;
+
+    private Long price;
+
+    public Item(Long id, String title, String pic, String desc, Long price) {
+        this.id = id;
+        this.title = title;
+        this.pic = pic;
+        this.desc = desc;
+        this.price = price;
+    }
+}
