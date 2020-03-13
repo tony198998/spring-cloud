00a7f795fcdcdddf34d10502418d6780fa0bb2b2
tony198998 <756903039@qq.com>
2020-03-13 11:05:55 +0800

    增加微服务网关和网关过滤器

--- cloud-order/src/main/java/com/wode/controller/OrderController.java
+++ cloud-order/src/main/java/com/wode/controller/OrderController.java
@@ -2,18 +2,21 @@ package com.wode.controller;
 
 import com.wode.entity.Order;
 import com.wode.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class OrderController {
     @Autowired
     private OrderService orderService;
 
-    @GetMapping(value = "order/{orderId}")
+    @GetMapping(value = "/order/{orderId}")
     public Order queryOrderById(@PathVariable("orderId") String orderId) {
+        log.info("8082--order"+orderId);
         return this.orderService.queryOrderById(orderId);
     }
 }
--- cloud-order/src/main/java/com/wode/service/ItemService.java
+++ cloud-order/src/main/java/com/wode/service/ItemService.java
@@ -4,12 +4,14 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.wode.feign.ItemFeignClient;
 import com.wode.entity.Item;
 import com.wode.properties.OrderProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@Slf4j
 public class ItemService {
 
     // Spring框架对RESTful方式的http请求做了封装，来简化操作
@@ -30,6 +32,7 @@ public class ItemService {
         // 该方法走eureka注册中心调用(去注册中心根据app-item查找服务，这种方式必须先开启负载均衡@LoadBalanced)
         String itemUrl = "http://app-item/item/{id}";
         Item result = itemFeignClient.queryItemById(id);
+        log.info("ORDER-service de result",result);
         return result;
     }
 
