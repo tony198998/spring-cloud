b63aa5b1bc5fa6fdbb208a5034aa7c946af0dee6
tony198998 <756903039@qq.com>
2020-03-13 14:14:07 +0800

    增加配置中心服务端

---
+++ cloud-config-server/src/main/java/com/wode/config/server/ConfigApplication.java
@@ -0,0 +1,16 @@
+package com.wode.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 开启配置服务
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
