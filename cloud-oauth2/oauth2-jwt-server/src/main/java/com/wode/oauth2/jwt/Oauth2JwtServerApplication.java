bae4130c94679e9df1fc13cb4a7dc8ae61a4b7fb
tony198998 <756903039@qq.com>
2020-03-17 17:25:21 +0800

    添加oauth2鉴权模块

---
+++ cloud-oauth2/oauth2-jwt-server/src/main/java/com/wode/oauth2/jwt/Oauth2JwtServerApplication.java
@@ -0,0 +1,13 @@
+package com.wode.oauth2.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2JwtServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2JwtServerApplication.class, args);
+    }
+
+}
---
+++ cloud-oauth2/oauth2-server/src/main/java/com/wode/oauth2/Oauth2ServerApplication.java
@@ -0,0 +1,13 @@
+package com.wode.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ServerApplication.class, args);
+    }
+
+}
